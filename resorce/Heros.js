960d118951b2c595a6b81ddc21595969a5d11d0a
Atiya <matana@medcon.com>
2019-04-02 13:38:23 +0300

    Adding new objs to the HEROS json

--- resorce/Heros.js
+++ resorce/Heros.js
@@ -1,22 +1,81 @@
-const heros = [
-    {
-      id: 1,
-      name: 'Mistre Glass',
-      power: 'Fire',
-      status: 'active'
-    },
-    {
-      id: 2,
-      name: 'The Hord',
-      power: 'Ice',
-      status: 'inactive'
-    },
-    {
-      id: 3,
-      name: 'Professor X',
-      power: 'Maind Controll',
-      status: 'active'
-    }
-  ];
-  
-  module.exports = heros;
+const heros = [{
+    id: 1,
+    name: 'Mistre Glass',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'The Hord',
+    power: 'Ice',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Professor X',
+    power: 'Maind Controll',
+    status: 'active'
+  },
+  {
+    id: 11,
+    name: 'Mr. Pbody',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 12,
+    name: 'The Gaint peach',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 13,
+    name: 'The Ripper',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 14,
+    name: 'Number Seven - The White Violin',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 15,
+    name: 'Number One - Spaceboy',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 16,
+    name: 'Number Two - The Kraken',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 17,
+    name: 'Number Three - The Rumor',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 18,
+    name: 'Number Four - The Séance',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 19,
+    name: 'Number Five - The Boy',
+    power: 'Fire',
+    status: 'active'
+  },
+  {
+    id: 20,
+    name: 'Number Six - The Horror',
+    power: 'Fire',
+    status: 'active'
+  }
+]
+
+module.exports = heros;
